1f787d1cc9340369f13414a3a0845989e1b50ddf
Bartek Szopka <bartek.szopka+github@gmail.com>
2016-11-24 13:39:55 +0100

    Added simple check of repository existence through GH API

--- src/common/components/repository-input/index.js
+++ src/common/components/repository-input/index.js
@@ -1,3 +1,5 @@
+import 'isomorphic-fetch';
+
 import React, { Component } from 'react';
 
 import parseGitHubUrl from 'parse-github-url';
@@ -39,7 +41,32 @@ export class RepositoryInput extends Component {
 
     if (repo) {
       let repoUrl = `https://github.com/${repo}.git`;
+      let apiUrl = `https://api.github.com/repos/${repo}`;
+
       message = <span>Repository: {repo}, URL: <a href={repoUrl}>{repoUrl}</a></span>;
+
+      let self = this;
+      fetch(apiUrl)
+        .then((response) => {
+          if (response.status >= 200 && response.status < 300) {
+            return response.json();
+          } else {
+            const error = new Error(response.statusText);
+            error.response = response;
+            throw error;
+          }
+        })
+        .then(() => {
+          self.setState({
+            message: <span>{this.state.message}  [exists on GitHub]</span>
+          });
+        })
+        .catch((errors) => {
+          self.setState({
+            message: `Repository ${repo} doesn't exist (${errors})`
+          });
+        });
+
     } else {
       message = 'Invalid repository URL';
     }
