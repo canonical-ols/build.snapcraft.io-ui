71016030686de13eda07425f5052ae1e501250a7
Colin Watson <cjwatson@canonical.com>
2016-12-02 17:57:38 +0000

    Add simple SSO authentication
    If OPENID_TEAMS is set (currently ["ubuntuone-hackers"] for staging and
    production), then we require the user to prove membership of at least
    one of those teams before we let them use the site.  This gives us a
    quick way to restrict widespread use before everything is ready.
    Long-term, we expect to require GitHub credentials instead.
    
    Almost all of this is cloned-and-hacked from javan-rhino.  The
    authorisation is hooked up slightly differently, since in this case
    we're using SSO as a front door rather than extracting any meaningful
    information from it.

--- src/common/containers/app.js
+++ src/common/containers/app.js
@@ -1,11 +1,16 @@
 import React, { PropTypes, Component } from 'react';
 import Helmet from 'react-helmet';
+import { connect } from 'react-redux';
 
 import Header from '../components/header';
 import Footer from '../components/footer';
+import Notification from '../components/notification';
+import styles from './app.css';
 
 export class App extends Component {
   render() {
+    const { oyez } = this.props;
+
     return (
       <div>
         <Helmet
@@ -17,7 +22,12 @@ export class App extends Component {
           ]}
         />
         <Header />
-        { this.props.children }
+        <div className={ styles.staticNotifications }>
+          { oyez && oyez.map((oy, i) => {
+            return <Notification key={i} { ...oy } />;
+          })}
+        </div>
+        { (!oyez || !oyez.length) && this.props.children }
         <Footer />
       </div>
     );
@@ -25,7 +35,18 @@ export class App extends Component {
 }
 
 App.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
+  oyez: PropTypes.array
 };
 
-export default App;
+function mapStateToProps(state) {
+  const {
+    oyez
+  } = state;
+
+  return {
+    oyez
+  };
+}
+
+export default connect(mapStateToProps)(App);
--- src/common/reducers/index.js
+++ src/common/reducers/index.js
@@ -2,9 +2,11 @@ import { routerReducer } from 'react-router-redux';
 import { combineReducers } from 'redux';
 
 import * as repositoryInput from '../reducers/repository-input';
+import * as oyez from '../reducers/oyez';
 
 const rootReducer = combineReducers({
   ...repositoryInput,
+  ...oyez,
   routing: routerReducer
 });
 
