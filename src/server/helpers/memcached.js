0a2ba18c40d0a69c1ef29221c7b9362872c269e1
Bartek Szopka <bartek.szopka+github@gmail.com>
2017-01-03 12:45:27 +0100

    Missing unsaved change

--- src/server/helpers/memcached.js
+++ src/server/helpers/memcached.js
@@ -43,7 +43,7 @@ export const getMemcached = () => {
 };
 
 // Test affordance
-export const initInMemoryMemcached = () => {
+export const setupInMemoryMemcached = () => {
   memcached = getInMemoryMemcachedStub();
 };
 
