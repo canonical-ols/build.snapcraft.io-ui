a138475439710e116739479095aa8d13f5f7838a
Stephen Stewart <stephen.stewart@carisenda.com>
2017-01-31 17:42:29 +0000

    sort builds in build list by id

--- src/common/components/build-history/index.js
+++ src/common/components/build-history/index.js
@@ -1,20 +1,23 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import sortBy from 'lodash/sortBy';
 
 import BuildRow from '../build-row';
 import { Message } from '../forms';
 
 const BuildHistory = (props) => {
-  const { repository, success } = props;
+  const { repository, success, builds } = props;
 
-  const builds = props.builds.map((build) => (
+  sortBy(builds, ['buildId']);
+
+  const buildRows = builds.map((build) => (
     <BuildRow key={build.buildId} {...build} repository={repository} />
   ));
 
   return success ? (
     <div>
-      {builds.length > 0
-        ? builds
+      {buildRows.length > 0
+        ? buildRows
         : <Message status='info'>This snap has not been built yet.</Message>
       }
     </div>
