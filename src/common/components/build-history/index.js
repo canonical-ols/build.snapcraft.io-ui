e662607216c2100ec84450b0eec1ec7aaeea6615
Stephen Stewart <stephen.stewart@carisenda.com>
2017-01-31 21:49:06 +0000

    actually sort builds by id, add some tests

--- src/common/components/build-history/index.js
+++ src/common/components/build-history/index.js
@@ -1,27 +1,29 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import sortBy from 'lodash/sortBy';
 
 import BuildRow from '../build-row';
 import { Message } from '../forms';
 
-const BuildHistory = (props) => {
+export const BuildHistory = (props) => {
   const { repository, success, builds } = props;
 
-  sortBy(builds, ['buildId']);
+  const hasBuilds = (builds && builds.length > 0);
 
-  const buildRows = builds.map((build) => (
-    <BuildRow key={build.buildId} {...build} repository={repository} />
-  ));
+  if (!success) {
+    return null;
+  }
 
-  return success ? (
-    <div>
-      {buildRows.length > 0
-        ? buildRows
-        : <Message status='info'>This snap has not been built yet.</Message>
-      }
-    </div>
-  ) : null;
+  if (!hasBuilds) {
+    return <Message status='info'>This snap has not been built yet.</Message>;
+  }
+
+  const buildRows = builds
+    .sort((a,b) => (+a.buildId < +b.buildId))
+    .map((build) => (
+      <BuildRow key={build.buildId} {...build} repository={repository} />
+    ));
+
+  return <div>{ buildRows }</div>;
 };
 
 BuildHistory.propTypes = {
@@ -30,7 +32,7 @@ BuildHistory.propTypes = {
     name: PropTypes.string
   }),
   success: PropTypes.bool,
-  builds: React.PropTypes.arrayOf(React.PropTypes.object)
+  builds: React.PropTypes.arrayOf(React.PropTypes.node)
 };
 
 function mapStateToProps(state) {
