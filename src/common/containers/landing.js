ad445a196fced169a4945a6577fdfb2be36098a1
Bartek Szopka <bartek.szopka+github@gmail.com>
2017-02-07 14:49:11 +0100

    Separating landing and dashboard routes and containers

---
+++ src/common/containers/landing.js
@@ -0,0 +1,43 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+
+import { Anchor } from '../components/button';
+
+import styles from './container.css';
+
+class Landing extends Component {
+  componentWillMount() {
+    if (this.props.auth.authenticated) {
+      this.props.router.replace('/repos');
+    }
+  }
+
+  render() {
+    return (
+      <div className={ styles.container }>
+        <h2>Auto-build and publish software for any Linux system.</h2>
+        <p>Just a placeholder landing page...</p>
+        <Anchor href="/auth/authenticate">Set Up in Minutes</Anchor>
+      </div>
+    );
+  }
+}
+
+Landing.propTypes = {
+  children: PropTypes.node,
+  auth: PropTypes.object,
+  router: PropTypes.object
+};
+
+function mapStateToProps(state) {
+  const {
+    auth
+  } = state;
+
+  return {
+    auth
+  };
+}
+
+export default connect(mapStateToProps)(withRouter(Landing));
