44af33e421b7178f08c4cd6b351f259195964b37
Stephen Stewart <stephen.stewart@carisenda.com>
2017-02-17 13:05:41 +0000

    add basic tests

---
+++ test/unit/src/common/components/help/t_install_snap.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import expect from 'expect';
+import { shallow } from 'enzyme';
+
+import HelpInstallSnap from '../../../../../../src/common/components/help/install-snap';
+
+describe('<HelpInstallSnap />', function() {
+
+  const name = 'needleinhaystack';
+  const revision = 14159265358979;
+
+  beforeEach(function() {
+  });
+
+  it('should include snap name', function() {
+    const wrapper = shallow(<HelpInstallSnap name={ name } revision={ revision } />);
+    expect(/needleinhaystack/.test(wrapper.text())).toExist();
+  });
+
+  it('should include revision number', function() {
+    const wrapper = shallow(<HelpInstallSnap name={ name } revision={ revision } />);
+    expect(/14159265358979/.test(wrapper.text())).toExist();
+  });
+});
