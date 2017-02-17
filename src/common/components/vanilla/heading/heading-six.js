ca9e5484d52bd30b7d78f9ce21b4b16065d2b256
Stephen Stewart <stephen.stewart@carisenda.com>
2017-02-17 12:23:04 +0000

    fix typo in heading-six

--- src/common/components/vanilla/heading/heading-six.js
+++ src/common/components/vanilla/heading/heading-six.js
@@ -3,9 +3,9 @@ import styles from './heading-six.css';
 
 export default function HeadingSix(props) {
   return (
-    <h4 className={ styles['heading-six'] }>
+    <h6 className={ styles['heading-six'] }>
       { props.children }
-    </h4>
+    </h6>
   );
 }
 
