3c4f37b85312788a63a7162094da10338e6867c3
Colin Watson <cjwatson@canonical.com>
2017-02-21 09:52:40 +0000

    Add actions for snap name registration
    These aren't hooked up to anything yet.

---
+++ src/common/reducers/register-name.js
@@ -0,0 +1,44 @@
+import * as ActionTypes from '../actions/register-name';
+
+export function registerName(state = {}, action) {
+  const { payload } = action;
+
+  const initialStatus = {
+    isFetching: false,
+    success: false,
+    error: null
+  };
+
+  switch (action.type) {
+    case ActionTypes.REGISTER_NAME:
+      return {
+        ...state,
+        [payload.id]: {
+          ...initialStatus,
+          isFetching: true
+        }
+      };
+    case ActionTypes.REGISTER_NAME_SUCCESS:
+      return {
+        ...state,
+        [payload.id]: {
+          ...state[payload.id],
+          isFetching: false,
+          success: true,
+          error: null
+        }
+      };
+    case ActionTypes.REGISTER_NAME_ERROR:
+      return {
+        ...state,
+        [payload.id]: {
+          ...state[payload.id],
+          isFetching: false,
+          success: false,
+          error: payload.error
+        }
+      };
+    default:
+      return state;
+  }
+}
