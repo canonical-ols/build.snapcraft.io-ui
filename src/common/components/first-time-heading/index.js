af4b6485bed7dea0533e4bdf4677ff151e5063bb
Bartek Szopka <bartek.szopka+github@gmail.com>
2017-02-21 14:32:14 +0000

    Properly render both message and progress based on context

--- src/common/components/first-time-heading/index.js
+++ src/common/components/first-time-heading/index.js
@@ -11,51 +11,52 @@ const hasStoreNameButNotSnapcraftData = (snap) => snap.store_name && !snap.snapc
 
 class FirstTimeHeading extends Component {
 
-  getHeadingText() {
+  getCurrentState() {
     const snapsStore = this.props.snaps;
     const snaps = snapsStore.snaps;
 
     let message = null;
+    let progress = null;
 
     if (snapsStore.success) {
       // no repos added yet
-
       if (snaps.length === 0) {
         message = 'Let’s get started! First, choose one or more GitHub repos for building.';
+        progress = [SIGNALS.DONE, SIGNALS.ACTIVE, SIGNALS.DEFAULT];
       // at least one repo, but none have a name yet
       } else if (snaps.filter(hasStoreName).length === 0) {
         message = 'Great! To publish a snap to the Store, it needs a unique name. Try registering one now.';
+        progress = [SIGNALS.DONE, SIGNALS.DONE, SIGNALS.ACTIVE];
       // at least one repo has a name but no snapcraft.yaml, and none have both
       } else if (snaps.filter(hasStoreNameButNotSnapcraftData).length &&
                  snaps.filter(hasStoreNameAndSnapcraftData).length === 0) {
         message = 'Okay, your repo is registered. Now push a snapcraft.yaml file, and building will start.';
+        progress = [SIGNALS.DONE, SIGNALS.DONE, SIGNALS.ACTIVE];
       }
       // TODO: bartaz add last check for build in one repository
     }
 
-    return message;
+    return {
+      message,
+      progress
+    };
   }
 
   // TODO: bartaz display state properly (not based on message)
-  renderProgress(message) {
-    return (message
-      ? <TrafficLights signalState={[ SIGNALS.DONE, SIGNALS.DONE, SIGNALS.ACTIVE ]} />
-      : null
-    );
+  renderProgress(progress) {
+    return (progress ? <TrafficLights signalState={ progress } /> : null);
   }
 
   renderMessage(message) {
-    return (message
-      ? <HeadingOne>{this.getHeadingText()}</HeadingOne>
-      : null);
+    return (message ? <HeadingOne>{message}</HeadingOne> : null);
   }
 
   render() {
-    const message = this.getHeadingText();
+    const { message, progress } = this.getCurrentState();
 
     return (
       <div className={styles.firstTimeHeading}>
-        { this.renderProgress(message) }
+        { this.renderProgress(progress) }
         { this.renderMessage(message) }
       </div>
     );
