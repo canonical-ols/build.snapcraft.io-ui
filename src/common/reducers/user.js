fe837ef912d4e66f93f090d31a0aebf2a6d51434
Bartek Szopka <bartek.szopka+github@gmail.com>
2017-02-28 10:34:38 +0100

    Added debugging action to set GH user

--- src/common/reducers/user.js
+++ src/common/reducers/user.js
@@ -1,4 +1,16 @@
 // initialised with server side rendered state
-export function user(state = null){
-  return state;
+
+export const DEBUG_SET_USER = 'DEBUG_SET_USER';
+
+export function user(state = null, action) {
+  switch(action.type) {
+    // set individual user properties for debugging
+    case DEBUG_SET_USER:
+      return {
+        ...state,
+        ...action.payload
+      };
+    default:
+      return state;
+  }
 }
