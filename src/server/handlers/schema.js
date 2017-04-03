5fb091dd996b851ae5861e0143864586dc948dcf
Stephen Stewart <stephen.stewart@carisenda.com>
2017-04-03 21:53:51 +0100

    pick only properties from github api we need

--- src/server/handlers/schema.js
+++ src/server/handlers/schema.js
@@ -1,9 +1,17 @@
 import { schema } from 'normalizr';
+import pick from 'lodash/pick';
 
 export const owner = new schema.Entity('owners');
 // XXX repository or repo?
 export const repo = new schema.Entity('repos', {
   owner: owner
+}, {
+  processStrategy: (entity) => pick(entity, [
+    'owner',
+    'id',
+    'full_name',
+    'html_url'
+  ])
 });
 export const repoList = new schema.Array(repo);
 
