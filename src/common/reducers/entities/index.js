6f9df01ee2e0aa137a234bde13c85e12f61792ea
Stephen <stephen.stewart@carisenda.com>
2017-04-06 14:43:17 +0200

    More test coverage for repository actions and reducers (#597)
    * entities default test
    
    * repository entity tests
    
    * Update t_entities.js
    
    * Update t_entities.js
    
    * keep entity collection reducers together
    
    * use repoPayload fixture

--- src/common/reducers/entities/index.js
+++ src/common/reducers/entities/index.js
@@ -1,27 +1,43 @@
 import merge from 'lodash/merge';
 
 import * as RepoActionTypes from '../../actions/repository';
-
 import repository from './repository';
-
 
 // TODO snaps
 
-export function entities(state = { snaps: {}, repos: {} }, action) {
+export function entities(state = {
+  snaps: {},
+  repos: {},
+  owners: {}
+}, action) {
   if (action.payload && action.payload.entities) {
     return merge({}, state, action.payload.entities);
   }
 
   // only modify repos if action is one of REPO_ types
-  if (action.payload && action.payload.id && RepoActionTypes[action.type]) {
-    return {
-      ...state,
-      repos: {
-        ...state.repos,
-        [action.payload.id]: repository(state.repos[action.payload.id], action)
-      }
-    };
+  if (RepoActionTypes[action.type]) {
+    return reduceRepoEntity(state, action);
   }
 
   return state;
+}
+
+function reduceRepoEntity(state, action) {
+  return reduceEntityHelper(state, action, repository, 'repos');
+}
+
+function reduceEntityHelper(state, action, reducer, name) {
+  const { payload } = action;
+
+  if (payload && payload.id) {
+    return {
+      ...state,
+      [name]: {
+        ...state[name],
+        [payload.id]: reducer(state[name][payload.id], action)
+      }
+    };
+  } else {
+    return state;
+  }
 }
