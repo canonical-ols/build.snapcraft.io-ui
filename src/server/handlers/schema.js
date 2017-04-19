7a0f403f44710f708f8895e9b5e6e7ff6f1f0765
Bartek Szopka <bartek.szopka+github@gmail.com>
2017-04-19 15:50:45 +0200

    Whitelisting snap properties returned by API to those needed on client-side

--- src/server/handlers/schema.js
+++ src/server/handlers/schema.js
@@ -18,6 +18,15 @@ export const repo = new schema.Entity('repos', {
 export const repoList = new schema.Array(repo);
 
 export const snap = new schema.Entity('snaps', {}, {
-  idAttribute: 'git_repository_url'
+  idAttribute: 'git_repository_url',
+  processStrategy: (entity) => {
+    return {
+      id: entity.git_repository_url,
+      git_repository_url: entity.git_repository_url,
+      self_link: entity.self_link,
+      snapcraft_data: entity.snapcraft_data,
+      store_name: entity.store_name
+    };
+  }
 });
 export const snapList = new schema.Array(snap);
