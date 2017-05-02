30012fc93ad925b042ba5d616170389a8363a778
Stephen Stewart <stephen.stewart@carisenda.com>
2017-05-02 14:42:50 +0100

    remove first, failed, attempt at fix

--- src/common/helpers/map-api-string.js
+++
@@ -1,8 +0,0 @@
-const STRINGS = {
-  'Successfully built': 'Built and published',
-  'Needs building': 'Building soon'
-};
-
-export default function mapApiString(lpString) {
-  return STRINGS.hasOwnProperty(lpString) ? STRINGS[lpString] : lpString;
-}
