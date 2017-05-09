e90e7722daeffbdcb9c96d59f2b00c48290a3d4f
Adam Collard <adam.collard@canonical.com>
2017-05-09 12:29:00 +0100

    Fix lint

--- mocks/service/github/responses.js
+++ mocks/service/github/responses.js
@@ -148,18 +148,20 @@ export function okayOrgsFound(req, res) {
   };
 
   res.set(headers).status(200).send([
-    {login: 'github',
-     id: 1,
-     url: 'https://api.github.com/orgs/github',
-     repos_url: 'https://api.github.com/orgs/github/repos',
-     events_url: 'https://api.github.com/orgs/github/events',
-     hooks_url: 'https://api.github.com/orgs/github/hooks',
-     issues_url: 'https://api.github.com/orgs/github/issues',
-     members_url: 'https://api.github.com/orgs/github/members{/member}',
-     public_members_url: 'https://api.github.com/orgs/github/public_members{/member}',
-     avatar_url: 'https://github.com/images/error/octocat_happy.gif',
-     description: 'A great organization'
-    }]);
+    {
+      login: 'github',
+      id: 1,
+      url: 'https://api.github.com/orgs/github',
+      repos_url: 'https://api.github.com/orgs/github/repos',
+      events_url: 'https://api.github.com/orgs/github/events',
+      hooks_url: 'https://api.github.com/orgs/github/hooks',
+      issues_url: 'https://api.github.com/orgs/github/issues',
+      members_url: 'https://api.github.com/orgs/github/members{/member}',
+      public_members_url: 'https://api.github.com/orgs/github/public_members{/member}',
+      avatar_url: 'https://github.com/images/error/octocat_happy.gif',
+      description: 'A great organization'
+    }
+  ]);
 }
 
 export function okayNoReposFound(req, res) {
