dc8ddbad6afd386be0f698c351a9398131a06612
Bartek Szopka <bartek.szopka+github@gmail.com>
2017-05-16 13:38:37 +0200

    Added copy button next to badge image

--- src/common/components/badge/index.js
+++ src/common/components/badge/index.js
@@ -3,16 +3,25 @@ import React, { PropTypes } from 'react';
 import { conf } from '../../helpers/config';
 const BASE_URL = conf.get('BASE_URL');
 
-const getBadgeUrl = (fullName) => `${BASE_URL}/badge/${fullName}.svg`;
+import { CopyToClipboard } from '../share';
 
-// TODO: (when we have UI to copy Markdown)
-// const getRepoPageUrl = (fullName) => `${BASE_URL}/user/${fullName}`;
-// const getBadgeMarkdown = (fullName) => `[![Snap Status](${getBadgeUrl(fullName)})](${getRepoPageUrl(fullName)})`;
+import style from './badge.css';
+
+const getBadgeUrl = (fullName) => `${BASE_URL}/badge/${fullName}.svg`;
+const getRepoPageUrl = (fullName) => `${BASE_URL}/user/${fullName}`;
+const getBadgeMarkdown = (fullName) => `[![Snap Status](${getBadgeUrl(fullName)})](${getRepoPageUrl(fullName)})`;
 
 export default function Badge({ fullName }) {
   return (
     <div>
       <img src={getBadgeUrl(fullName)} alt={`Snap build status for ${fullName}`} />
+
+      <span className={style.copyIcon}>
+        <CopyToClipboard
+          tooltip="Copy Markdown code to clipboard"
+          copyme={getBadgeMarkdown(fullName)}
+        />
+      </span>
     </div>
   );
 }
