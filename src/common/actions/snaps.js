fdfb39acebca4349d519b1126aaba2acc6770cbe
Bartek Szopka <bartek.szopka+github@gmail.com>
2017-05-29 16:05:45 +0200

    Added redux actions to handle checking stable release status

--- src/common/actions/snaps.js
+++ src/common/actions/snaps.js
@@ -8,6 +8,9 @@ export const FETCH_SNAPS_ERROR = 'FETCH_SNAPS_ERROR';
 export const REMOVE_SNAP = 'REMOVE_SNAP';
 export const REMOVE_SNAP_SUCCESS = 'REMOVE_SNAP_SUCCESS';
 export const REMOVE_SNAP_ERROR = 'REMOVE_SNAP_ERROR';
+export const FETCH_SNAP_DETAILS = 'FETCH_SNAP_DETAILS';
+export const FETCH_SNAP_DETAILS_SUCCESS = 'FETCH_SNAP_DETAILS_SUCCESS';
+export const FETCH_SNAP_DETAILS_ERROR = 'FETCH_SNAP_DETAILS_ERROR';
 
 export function fetchUserSnaps(owner) {
   const query = `owner=${encodeURIComponent(owner)}`;
@@ -34,6 +37,22 @@ export function removeSnap(repositoryUrl) {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ repository_url: repositoryUrl }),
+        credentials: 'same-origin'
+      }
+    }
+  };
+}
+
+export function fetchSnapStableRelease(repositoryUrl, snapName) {
+  const query = 'channel=stable&fields=name,revision';
+
+  return {
+    payload: { id: repositoryUrl },
+    [ CALL_API ]: {
+      types: [ FETCH_SNAP_DETAILS, FETCH_SNAP_DETAILS_SUCCESS, FETCH_SNAP_DETAILS_ERROR ],
+      path: `/api/snaps/details/${encodeURIComponent(snapName)}?${query}`,
+      options: {
+        headers: { 'Content-Type': 'application/json' },
         credentials: 'same-origin'
       }
     }
